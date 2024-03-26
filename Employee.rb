91ffa644dc8079a0396af6c878c9b2318353f1b5
Dharani <dharani1414@gmail.com>
2024-03-26 12:47:14 -0700

    total salary method

--- Employee.rb
+++ Employee.rb
@@ -1,15 +1,19 @@
+
 class Employee
+
+    attr_reader :name, :title, :salary, :boss, :employees
 
     def initialize(name, title, salary, boss)
         @name = name
         @title = title
         @salary = salary
         @boss = boss
+        @boss.employees << self if @boss != nil
+        @employees = []
     end
 
+
     def bonus(multiplier)
-        if @title != "manager"
-            bonus = @salary * multiplier
-        end
+        bonus = @salary * multiplier 
     end
 end
--- Manager.rb
+++ Manager.rb
@@ -1,6 +1,33 @@
+require_relative 'Employee.rb'
+
 class Manager < Employee
-    def initialize(employee)
+    attr_reader :name, :title, :salary, :boss, :employees
+
+    def initialize(name, title, salary, boss)
+        @name = name
+        @title = title
+        @salary = salary
+        @boss = boss
+        @boss.employees << self if @boss != nil
         @employees = []
+    end
+
+    def add_employee(employee)
         @employees << employee
+    end
+    def total_salary
+        salary = 0
+       @employees.each do |employee| 
+            if employee.is_a?(Manager)
+               salary += employee.salary
+               salary += employee.total_salary
+            else
+                salary += employee.salary
+            end
+        end
+       salary
+    end
+    def bonus(multiplier)
+      self.total_salary * multiplier
     end
 end
